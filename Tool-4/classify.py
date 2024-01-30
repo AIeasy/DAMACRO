a96ca4efc403a91dde1e619da578883a016abd27
Zhongxin Hu <578981654@qq.com>
2024-01-31 03:47:13 +0800

    Update classify.py

--- Tool-4/classify.py
+++ Tool-4/classify.py
@@ -3,14 +3,14 @@ import warnings
 import numpy as np
 import json
 from sklearn.preprocessing import LabelEncoder
+import joblib
 def fd_json_read(config_file):
     with open(config_file, 'r') as file:
         config = json.load(file)
     cat_col = config["column_cat_col"]
     col_drop = config["column_drop"]
-    delimiter = config["delimiter"]
 
-    return cat_col, col_drop, delimiter
+    return cat_col, col_drop
 
 def preprocess_data_fd(df,cat_col,drop_col):
     le = LabelEncoder()
@@ -23,27 +23,31 @@ def preprocess_data_fd(df,cat_col,drop_col):
     
     df = df.drop(df.columns[drop_col], axis=1)
     df.replace('', np.nan, inplace=True)
-    df = df.fillna
+    df = df.fillna("0")
     return df
 def preprocess_data(df):
     numeric_columns = df.select_dtypes(include='number')
     return numeric_columns
-def classify_data(df, model):
-    predictions = model.predict(df)
+def classify_data(df, model,scaler_path):
+
+    scaler = joblib.load(scaler_path)
+    df_scaled = scaler.transform(df)
+    predictions = model.predict(df_scaled)
     return predictions
 def add_labels_to_chunk(chunk, labels):
     chunk['label'] = labels
     return chunk
 def classify_chunk(chunk,model):
-    warnings.filterwarnings("ignore")
+    #warnings.filterwarnings("ignore")
     processed_data = preprocess_data(chunk)
     predictions = classify_data(processed_data, model)
     chunk_with_labels = add_labels_to_chunk(chunk, predictions)
     return chunk_with_labels
-def classify_chunk_fd(chunk,model,json_path):
+def classify_chunk_fd(chunk,model,json_path,scaler_path):
     warnings.filterwarnings("ignore")
-    cat_col,drop_col,delimiter = fd_json_read(json_path)
+    cat_col,drop_col = fd_json_read(json_path)
     processed_data = preprocess_data_fd(chunk,cat_col,drop_col)
-    predictions = classify_data(processed_data, model)
+    #print("out preprocess")
+    predictions = classify_data(processed_data, model,scaler_path)
     chunk_with_labels = add_labels_to_chunk(chunk, predictions)
     return chunk_with_labels
