cd9e6a6ec6ae8a9cb10b2b34f3125cae0ef76fe7
Zhongxin Hu <578981654@qq.com>
2024-02-02 22:10:12 +0800

    Update main.py

--- Tool-4/main.py
+++ Tool-4/main.py
@@ -37,6 +37,7 @@ def get_folder_size(folder_path):
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
             total_size += os.path.getsize(file_path)
+            os.remove(file_path)
 
     return total_size / (1024 * 1024)
 def calculate_throughput(
