3cd0607e7ca519e3430406894f21dd4301dc4018
Zhongxin Hu <578981654@qq.com>
2024-02-02 22:50:58 +0800

    Update main.py

--- Tool-3/main.py
+++ Tool-3/main.py
@@ -14,7 +14,6 @@ def calculate_cost(
         compression_ratio: float,
         original_size: float,
         num_cores: int,
-        cost_scale = 'TB', # calculate the cost of handreds of TBs
         p_cpu = 0.048,
         p_net = 0.05,
 ):
@@ -29,6 +28,7 @@ def get_folder_size(folder_path):
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
             total_size += os.path.getsize(file_path)
+            os.remove(file_path)
 
     return total_size / (1024 * 1024)
 def calculate_throughput(
@@ -55,7 +55,6 @@ def compress(data, compression_algorithm):
         column_data = data[column].tolist()
         column_data.insert(0,column)
         column_data = pd.DataFrame(column_data)
-        #column_data = "  "
         if compression_algorithm == 'lz4':
             compressed_data = lz4.frame.compress(column_data.to_csv(index=False).encode())
             compressed_columns[column] = compressed_data
@@ -73,7 +72,8 @@ def compress(data, compression_algorithm):
     compress_time = end_time - start_time
 
     return compressed_columns, compress_time
-def transfer_process(worker_id, transfer_input_queue,output_queue,compression_algorithm,s_path, remote_username, remote_host, remote_file_path):
+def transfer_process(worker_id, transfer_input_queue,output_queue,compression_algorithm,s_path, remote_username, remote_host, remote_file_path,network_speed):
+    network_speed = network_speed*1024*8
     print(f'transfer_{worker_id}_start\n')
     save_folder = os.path.join(s_path, compression_algorithm)#create folder
     if not os.path.exists(save_folder):
@@ -96,13 +96,16 @@ def transfer_process(worker_id, transfer_input_queue,output_queue,compression_al
                 for filename in os.listdir(save_folder):#loop throgh all columns files saved in folder
                     file_path = os.path.join(save_folder, filename)
                     if (f'worker_{worker_id}' in filename):
-                        #print(f'transfer_{worker_id} is sending last pice\n')
                         #transfer_file_via_ssh(local_file_path=output_path, remote_username=remote_username, remote_host=remote_host, remote_file_path=remote_file_path)#transfer it
-                        cmd = ["scp", file_path, f"{remote_username}@{remote_host}:{remote_file_path}"]
+                        
+                        cmd = ["scp", '-l',f'{network_speed}',file_path, f"{remote_username}@{remote_host}:{remote_file_path}"]
                         subprocess.run(cmd)
-                        #print(f'transfer_{worker_id} done sending last pice\n')
+                        
+                        print(f'transfer_{worker_id} done sending last pice\n')
                 end_transfer_time = time.time()
                 process_transfer_time += end_transfer_time - start_transfer_time
+
+                #print('debugging')
                 output_queue.put((None,total_compress_time,process_transfer_time))#transfer is finished
                 print(f'transfer_{worker_id}_stopped_l\n')
                 break
@@ -119,22 +122,7 @@ def transfer_process(worker_id, transfer_input_queue,output_queue,compression_al
                 compressed_data = compressed_dic[key]#get the column data
                 with open(output_path, 'ab') as f:
                     f.write(compressed_data)#create a compressed file for that column
-            '''
-            with open(output_gay,'w') as f: # testing the order of file generation
-                f.write("gay")
             
-            start_transfer_time = time.time() #if you want pass the file everytime it updates
-            for filename in os.listdir(save_folder):#loop throgh all columns files saved in folder
-                file_path = os.path.join(output_path, filename)
-                if (f'worker_{worker_id}' in filename):
-                    print(f'transfer_{worker_id} is sending\n')
-                    transfer_file_via_ssh(local_file_path=1, remote_username=1, remote_host=1, remote_file_path=1)#transfer it
-                    print(f'transfer_{worker_id} done sending\n')
-            end_transfer_time = time.time()
-            process_transfer_time += end_transfer_time - start_transfer_time
-
-            output_queue.put((total_compress_time,process_transfer_time))#feed back the output
-            '''
 def worker_process(worker_id, input_queue, output_queue,alg,path):
     print(f'worker_{worker_id}_start\n')
     total_compress_time = 0
@@ -144,9 +132,7 @@ def worker_process(worker_id, input_queue, output_queue,alg,path):
         if data is None:  # Shutdown signal
             if not accumulated_data.empty:
                 compressed_data,compress_time = compress(accumulated_data,alg)#get leftover compressed_dic
-                #transfer_time = transfer(save_folder)
                 total_compress_time += compress_time
-                #total_transfer_time += transfer_time
                 output_queue.put((None,worker_id, total_compress_time,compressed_data))#feed the leftover compressed_dic into transfer and tell transfer to stop
                 print(f'worker_{worker_id}_end_l\n')
                 break
@@ -158,9 +144,7 @@ def worker_process(worker_id, input_queue, output_queue,alg,path):
         if len(accumulated_data) >= 10000:
 
             compressed_data,compress_time = compress(accumulated_data,alg)#get compressed_dic
-           # transfer_time = transfer(save_folder)
             total_compress_time += compress_time
-           # total_transfer_time += transfer_time
             output_queue.put((worker_id, total_compress_time,compressed_data))#feed the compressed_dic into transfer
             accumulated_data = pd.DataFrame()
 def classify_module(input_queue, output_queue,model):
@@ -177,13 +161,13 @@ def classify_module(input_queue, output_queue,model):
         output_queue.put(labeled_data)
     print("classify module is done")
     return
-def compress_module(input_queue, output_queue,alg,path,num_worker, remote_username, remote_host, remote_file_path):
+def compress_module(input_queue, output_queue,alg,path,num_worker, remote_username, remote_host, remote_file_path,network_speed):
     print("in the compress module")
 
     worker_queues = {label: Queue() for label in range(num_worker)}  # for number of worker, create worker queues
     transfer_queues = {label: Queue() for label in range(num_worker)}#for number of worker, create transfer queues
     workers = [Process(target=worker_process, args=(label, worker_queues[label], transfer_queues[label],alg,path)) for label in range(num_worker)]#create worker process
-    transfers = [Process(target=transfer_process, args=(label, transfer_queues[label],output_queue,alg,path, remote_username, remote_host, remote_file_path)) for label in range(num_worker)] #create transfer process
+    transfers = [Process(target=transfer_process, args=(label, transfer_queues[label],output_queue,alg,path, remote_username, remote_host, remote_file_path,network_speed)) for label in range(num_worker)] #create transfer process
 
     for worker in workers:#starting worker listening for labeled chunk
         worker.start()
@@ -203,18 +187,7 @@ def compress_module(input_queue, output_queue,alg,path,num_worker, remote_userna
         for label in range(num_worker):
             cluster_data =labeled_chunk.loc[labeled_chunk['label'] == label]
             worker_queues[label].put(cluster_data)
-    '''
-    while True:#listening for output 
-        output = output_queue.get()
-        if len(output)== 3:
-            _,compress_time, transfer_time = output#get output from transferer proce
-            total_compress_time += compress_time
-            total_transfer_time += transfer_time
-            break
-        compress_time, transfer_time = output#get output from transferer process
-        total_compress_time += compress_time
-        total_transfer_time += transfer_time
-        '''
+
     total_transfer_time =0
     start_time_c = time.time()#timing for compression time
     for worker in workers:#wait worker finish it jobs
@@ -234,24 +207,21 @@ def compress_module(input_queue, output_queue,alg,path,num_worker, remote_userna
     output_queue.put((compress_time, total_transfer_time,classify_time,None))
     print("out compress model")
     return
-def expierment(file_path,file_name,original_data_size,train_percent,model_path,model_name,chunk_size,algorithm,worker_num,targe_tip,target_user,network_speed,compress_save_path,target_path):
+
+# random split
+def base_line(file_name,original_data_size,train_percent,model_name,chunk_size,algorithm,worker_num,targe_tip,target_user,network_speed,compress_save_path,target_path):
     classify_queue = Queue()
     compress_queue = Queue()
     transfer_queue = Queue()
-    model = joblib.load(f'{model_path}/{train_percent}%_train/{model_name}_{file_name}.joblib')
-    classify_process = Process(target=classify_module, args=(classify_queue, compress_queue,model))
     compress_process = Process(target=compress_module, args=(compress_queue, transfer_queue,algorithm,compress_save_path,worker_num,target_user,targe_tip,target_path))
-    classify_process.start()#start listening for data stream for classification
     compress_process.start()#start listening labeled chunks for compression and transfer
-    set_network_conditions("ens33", f'{network_speed}mbit', "0ms", "0%")#set the network speed
+    #set_network_conditions("enp70s0", f'{network_speed}mbit', "0ms", "0%")#set the network speed
     print("loading data stream")
-    for  i,chunk in enumerate(pd.read_csv(f'{file_path}/{file_name}.csv', chunksize=chunk_size, delimiter='|')):
+    for  i,chunk in enumerate(pd.read_csv(f'{"data/original"}/{file_name}.csv', chunksize=chunk_size, delimiter='|')):
         classify_queue.put(chunk)
+
+    classify_queue.put(None, 0)
     print("stream loaded")
-    classify_queue.put(None)  # End of data stream signal
-    print("classify start")
-    classify_process.join() #wait the calssification module finish its jobs
-    print("classify finish")
     print("compress start")
     compress_process.join() #wait the compression module finish its jobs
     print("compress done")
@@ -271,22 +241,110 @@ def expierment(file_path,file_name,original_data_size,train_percent,model_path,m
     print("OUT: Throughput: ",throughput)
     cost = calculate_cost(compression_ratio=compression_ratio,original_size=original_data_size,num_cores=24,compression_time=total_compress_time)
     print("OUT: Cost: ",cost)
-    reset_network_conditions('eth0')
+    #reset_network_conditions('eth0')
+    return
+def expierment( file_path,file_name,original_data_size,model_path,train_percent,model_name,chunk_size,algorithm,worker_num,targe_tip,target_user,network_speed,compress_save_path,target_path,num_cores):
+    result_folder = os.path.join('/home/yunfei/Tool-3/results', f'{train_percent}%')#create folder
+    result_folder = os.path.join(result_folder,f'{file_name}')
+    if not os.path.exists(result_folder):
+        os.makedirs(result_folder)
+    classify_queue = Queue()
+    compress_queue = Queue()
+    transfer_queue = Queue()
+    model = joblib.load(f'{model_path}/{train_percent}%_train/{model_name}_{file_name}.joblib')
+    classify_process = Process(target=classify_module, args=(classify_queue, compress_queue,model))
+    compress_process = Process(target=compress_module, args=(compress_queue, transfer_queue,algorithm,compress_save_path,worker_num,target_user,targe_tip,target_path,network_speed))
+    classify_process.start()#start listening for data stream for classification
+    compress_process.start()#start listening labeled chunks for compression and transfer
+    #set_network_conditions("enp70s0", f'{network_speed}mbit', "0ms", "0%")#set the network speed
+    print("loading data stream")
+    for  i,chunk in enumerate(pd.read_csv(f'{file_path}/{file_name}.csv', chunksize=chunk_size, delimiter='|')):
+        classify_queue.put(chunk)
+    print("stream loaded")
+    classify_queue.put(None)  # End of data stream signal
+    print("classify start")
+    classify_process.join() #wait the calssification module finish its jobs
+    print("classify finish")
+    print("compress start")
+    compress_process.join() #wait the compression module finish its jobs
+    print("compress done")
+
+    i=0
+    while transfer_queue.empty() != True:#load the outputs
+        check = transfer_queue.get()
+        print("i=", i)
+        i += 1
+        if type(check) != None and len(check)==4:
+            
+            total_compress_time,total_transfer_time,classify_time,_ = check
+    print("OUT: Compressiontime: {:.4f}".format(total_compress_time))
+    print("OUT: TransferTime: {:.4f}".format(total_transfer_time))
+    print("OUT: Classification time: {:.4f}".format(classify_time))
+    compressed_size = get_folder_size(os.path.join(compress_save_path, algorithm))
+    print("OUT: Compressed size: {:.4f}".format(compressed_size))
+    compression_ratio =  original_data_size / compressed_size
+    print("OUT: Compression ratio: {:.4f}".format(compression_ratio))
+    throughput= calculate_throughput(classification_time=classify_time,compression_time=total_compress_time,compression_ratio=compression_ratio,network_speed=5,data_size=original_data_size)
+    print("OUT: Throughput: {:.4f}".format(throughput))
+    cost = calculate_cost(compression_ratio=compression_ratio,original_size=original_data_size,num_cores=num_cores,compression_time=total_compress_time)
+    print("OUT: Cost: {:.4f}".format(cost))
+    #reset_network_conditions('enp70s0')
+    with open(f'{result_folder}/result_{file_name}_{model_name}_{algorithm}_{network_speed}.txt', "w") as result_file:
+        result_file.write(f"Compressiontime: {total_compress_time:.4f}\n")
+        result_file.write(f"TransferTime: {total_transfer_time:.4f}\n")
+        result_file.write(f"Classification time: {classify_time:.4f}\n")
+        result_file.write(f"Compressed size: {compressed_size:.4f}\n")
+        result_file.write(f"Compression ratio: {compression_ratio:.4f}\n")
+        result_file.write(f"Throughput: {throughput:.4f}\n")
+        result_file.write(f"Cost: {cost:.4f}\n")
+        result_file.write(f"NetworkSpeed: {network_speed:.4f}\n")
+        result_file.write(f"Original_size: {original_data_size:.4f}\n")
+        result_file.write(f"Model: {model_name}\n")
+
     return
 
 def main():
+    '''
     original_data_size=151.3
-    file_path = 'data/original'
+    file_path = '/home/yunfei/Project/data/original'
     file_name = 'econbiz'
     train_percent = 20
-    model_path = 'models'
+    model_path = '/home/yunfei/Project/models'
     model_name = 'AdaBoost'
     alg = 'gzip'
-    networkspeed = 100
+    num_cores = 48
+    networkspeed = 2
     chunk_size = 10000
-    compress_save_path = f'data/compressed_data/{train_percent}%_train/{model_name}_{file_name}'
-    target_path = '/home/sean/Desktop/'
-    expierment(file_path,file_name,original_data_size,train_percent=20,model_path,model_name=model_name,chunk_size=chunk_size,algorithm=alg,worker_num=10,targe_tip='192.168.204.128',target_user='sean',network_speed=100,compress_save_path=compress_save_path,target_path=target_path)
+    compress_save_path = f'/home/yunfei/Tool-3/data/compressed_data/{train_percent}%_train/{model_name}_{file_name}'
+    target_path = '/eecs/home/zhongxin/target'
+
+    target_ip = 'brain.eecs.yorku.ca'
+    target_user = 'zhongxin'
+    expierment(file_path,file_name,original_data_size,model_path,train_percent=20,model_name=model_name,chunk_size=chunk_size,algorithm=alg,worker_num=10,targe_tip=target_ip,target_user=target_user,network_speed=networkspeed,compress_save_path=compress_save_path,target_path=target_path,num_cores=num_cores)
+
+    '''
+    original_data_size=151.3
+    file_path = '/home/yunfei/Project/data/original'
+    file_name = 'orders'
+    train_percent = 20
+    model_path = '/home/yunfei/Project/models'
+    model_names = ['AdaBoost','DecisionTree','RandomForest','KNN','LogisticRegression','MLP']
+    alg = 'gzip'
+    num_cores = 48
+    networkspeeds = [25,20,15,10,5,2]
+    chunk_size = 10000
+
+    target_path = '/eecs/home/zhongxin/target'
+
+    target_ip = 'brain.eecs.yorku.ca'
+    target_user = 'zhongxin'
+    for i in range(len(model_names)):
+        for j in range(len(networkspeeds)):
+            model_name = model_names[i]
+            networkspeed = networkspeeds[j]
+            compress_save_path = f'/home/yunfei/Tool-3/data/compressed_data/{train_percent}%_train/{model_name}_{file_name}'
+            expierment(file_path,file_name,original_data_size,model_path,train_percent=20,model_name=model_name,chunk_size=chunk_size,algorithm=alg,worker_num=10,targe_tip=target_ip,target_user=target_user,network_speed=networkspeed,compress_save_path=compress_save_path,target_path=target_path,num_cores=num_cores)
+
     return
 
 if __name__ == "__main__":
